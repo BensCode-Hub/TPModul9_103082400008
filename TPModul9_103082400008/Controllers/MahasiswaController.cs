d6f8f9a9bdba385dbd26d2c95acaf9ffe14e9238
BensCode <ruben.simarmata2@gmail.com>
2026-04-26 23:17:36 +0700

    Menambahkan API Mahasiswa

---
+++ TPModul9_103082400008/Controllers/MahasiswaController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using TPModul9_103082400008;
+
+namespace MahasiswaController.Controllers
+{
+    [Route("api/mahasiswa")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static LinkedList<Mahasiswa> listMahasiswa = new LinkedList<Mahasiswa>(new[]
+        {
+            new Mahasiswa { nama = "Ruben", NIM = "103082400008" },
+            new Mahasiswa { nama = "Joko", NIM = "109078492181" },
+            new Mahasiswa { nama = "Cheline", NIM = "873569835108" }
+        });
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return listMahasiswa;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return listMahasiswa.ElementAt(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa mhs)
+        {
+            listMahasiswa.AddLast(mhs);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var target = listMahasiswa.ElementAt(id);
+            listMahasiswa.Remove(target);
+        }
+    }
+}
+
---
+++ TPModul9_103082400008/Mahasiswa.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata;
+
+namespace TPModul9_103082400008
+{
+    public class Mahasiswa
+    {
+        public string nama { get; set; }
+        public string NIM { get; set; }
+    }
+}
